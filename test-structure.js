b816a4ad8c54f85cfb51f1576664424f089b5664
Damini <104028961+Daminidhon@users.noreply.github.com>
2023-08-27 18:21:03 +0530

    Update test-structure.js

--- test-structure.js
+++ test-structure.js
@@ -1,26 +1,45 @@
 import { Selector, fixture, test } from "testcafe"; // first imported testcafe selectors
 
-fixture `LoginPage` // declare the fixture
+fixture `Login Page` // declare the fixture
 .page `https://dev.deepthought.education/login`; // specify the start page of the given website
 
 //then created a test and placed code within it
-
-test('test with valid credentials', async t => {
+test('Test successful login with vaild creedetials.', async t => {
 
     await t 
     .typeText(Selector('#username'), 'daminidhondge')
     .typeText(Selector('#password'), 'Deep@123')
     .click(Selector('.btn'))
-    .expect(Selector('h5').withExactText('Welcome to DeepThought').exists).ok(); 
+   
     // Used the assertion to check if actual header text equals expected text
 
 })
 
-test('test with Invalid credentials', async t => {
+test('test with Invalid credentials.', async t => {
 
     await t 
     .typeText(Selector('#username'), 'daminidhondge')
     .typeText(Selector('#password'), 'Deep@2023')
     .click(Selector('.btn'))
-    .expect(Selector('#login-error-notify').innerText).eql('Login Unsuccessful');
+    
+})
+
+test('Validate that appropriate error messages are displayed for invalid login attempts.', async t => {
+
+    await t 
+    .typeText(Selector('#username'), 'daminidhondge')
+    .typeText(Selector('#password'), 'Deep@2023')
+    .click(Selector('.btn'))
+    .expect(Selector('p').withExactText('Invalid login credentials').exists).ok(); 
+     
+})
+
+test('On successful login, validate that the user is redirected to the dashboard screen.', async t => {
+
+    await t 
+    .typeText(Selector('#username'), 'daminidhondge')
+    .typeText(Selector('#password'), 'Deep@123')
+    .click(Selector('.btn'))
+    .expect(Selector('h5').withExactText('Welcome to DeepThought').exists).ok();    
+    
 })
